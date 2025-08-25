de549643e2fd0d1a50ae0923fcfe6b5639e8d786
LanzDesign <128187584+LanzDesign@users.noreply.github.com>
2025-08-26 00:18:01 +0200

    sanity

--- src/components/sanityClient.js
+++ src/components/sanityClient.js
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = createClient({
-  projectId: "weg2bpfw",   // findest du in sanity.config oder manage.sanity.io
+  projectId: "ru6syca8",   // findest du in sanity.config oder manage.sanity.io
   dataset: "production",          // oder den Namen deines Datasets
   apiVersion: "2025-01-01",       // fix auf ein Datum setzen, nicht "v1"
   useCdn: true,                   // true = schnell, aber evtl. gecached
